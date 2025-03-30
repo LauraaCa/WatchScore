38f023c70090a0622353d173786255da9da67557
LauraaCa <lauracastanedar962@gmail.com>
2025-03-30 13:55:08 -0500

    feature(bkend)validar usuario con contraseña encriptda

--- src/main/java/arquitectura/WatchScore/servicios/UsuarioServicio.java
+++ src/main/java/arquitectura/WatchScore/servicios/UsuarioServicio.java
@@ -60,15 +60,4 @@ public class UsuarioServicio {
         return Period.between(fechaNacimiento, hoy).getYears() >= 12;
     }
 
-    public Usuario autenticacion(String email, String contrasena) {
-        Usuario usuario = usuarioRepositorio.findByEmail(email);
-
-        if (usuario != null && BCrypt.checkpw(contrasena, usuario.getContrasena())) {
-            return usuario;
-        }
-
-        return null;
-    }
-
-
 }
