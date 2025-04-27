fc8116488346f0489b5be66e17df14f058e60a7d
LauraaCa <lauracastanedar962@gmail.com>
2025-04-27 13:26:16 -0500

    feature(bkend) : listas mejoradas

--- src/main/java/arquitectura/WatchScore/controladores/ListaControlador.java
+++ src/main/java/arquitectura/WatchScore/controladores/ListaControlador.java
@@ -1,11 +1,13 @@
 package arquitectura.WatchScore.controladores;
 
-
 import arquitectura.WatchScore.dto.ListaDTO;
 import arquitectura.WatchScore.persistencia.entidades.Lista;
 import arquitectura.WatchScore.servicios.ListaServicio;
+import jakarta.servlet.http.HttpSession;
 import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 
@@ -13,30 +15,38 @@ import java.util.List;
 @RequestMapping("/listas")
 @AllArgsConstructor
 @CrossOrigin(origins = "*")
-
 public class ListaControlador {
     ListaServicio listaServicio;
 
-    @PostMapping("/usuario/{usuarioId}")
-    public ListaDTO crearLista(@PathVariable Long usuarioId, @RequestBody ListaDTO lista) {
-       return listaServicio.crearLista(usuarioId, lista);
-    }
+    @PostMapping("/usuario")
+    public ListaDTO crearLista(@RequestBody ListaDTO lista, HttpSession session) {
+        Long usuarioId = (Long) session.getAttribute("idusuario");
 
+        if (usuarioId == null) {
+            throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "Usuario no autenticado");
+        }
+
+        return listaServicio.crearLista(usuarioId, lista);
+    }
 
     @PostMapping("/agregar/{listaId}/peliculas/{peliculaId}")
     public ListaDTO agregarPeliculaALista(@PathVariable Long listaId, @PathVariable Long peliculaId) {
-       return listaServicio.agregarPeliculaALista(listaId, peliculaId);
+        return listaServicio.agregarPeliculaALista(listaId, peliculaId);
     }
 
+    @GetMapping("/mis")
+    public List<Lista> obtenerListasPorUsuario(HttpSession session) {
+        Long usuarioId = (Long) session.getAttribute("idusuario");
 
-    @GetMapping("/mis/{usuarioId}")
-    public List<Lista> obtenerListasPorUsuario(@PathVariable Long usuarioId) {
+        if (usuarioId == null) {
+            throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "Usuario no autenticado");
+        }
+
         return listaServicio.obtenerListasPorUsuario(usuarioId);
     }
 
     @GetMapping("/todo")
-    public List<ListaDTO> todo(){
+    public List<ListaDTO> todo() {
         return listaServicio.todo();
     }
-
 }
